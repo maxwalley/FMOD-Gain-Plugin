ca4db08daab01ebd037d845ad97bb3a22e9f04b0
KingWallmo <max.walley1@btinternet.com>
2020-05-14 17:25:49 +0100

    Added Javascript file to manipulate the look of the plugin. Not currently working fully - shows a .png image but no fader/dial for the gain param

---
+++ Javascript/Script.plugin.js
@@ -0,0 +1,48 @@
+﻿studio.plugins.registerPluginDescription("MaxWalley Gain", {
+
+    productName: "Gain Plugin",
+
+    parameters: {
+        "gain": { displayName: "Volume" },
+    },
+
+    defaultConfigurations: [
+        {
+            name: "Half Gain",
+            parameterValues: {
+                "gain": -40,
+            },
+        },
+        {
+            name: "Full Gain",
+            parameterValues: {
+                "gain": 0,
+            },
+        },
+    ],
+
+    deckUi: {
+        deckWidgetType: studio.ui.deckWidgetType.Layout,
+        layout: studio.ui.layoutType.GridLayout,
+        contentsMargin: {
+            left: 5, top: 5, right: 5, bottom: 5,
+        },
+        minimumWidth: 200,
+        spacing: 5,
+        items: [
+            {
+                deckWidgetType: studio.ui.deckWidgetType.Pixmap,
+                filePath: __dirname + "/600px-Test_grid_4000x4000",
+                row: 1,
+                column: 1,
+                rowSpan: 1,
+                columnSpan: 1,
+            },
+
+            {
+                deckWidgetType: studio.ui.deckWidgetType.Fader,
+                binding: "gain",
+            },
+                ],
+    },
+});
